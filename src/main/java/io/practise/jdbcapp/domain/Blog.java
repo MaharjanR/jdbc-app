c619cc55cd8f311e38abe7b1a0cc372d74607a10
Ritesh Maharjan <rite_maharjan@yahoo.com>
2020-11-24 16:27:01 -0500

    Changed path of the table

---
+++ src/main/java/io/practise/jdbcapp/domain/Blog.java
@@ -0,0 +1,18 @@
+package io.practise.jdbcapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Blog {
+
+    private int userId;
+    private String blogName;
+    private int blogCategory;
+    private String blogTest;
+}
