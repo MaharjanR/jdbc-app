8206bc542f62cf7cadda09a40abb7a32d73e38ef
Ritesh Maharjan <rite_maharjan@yahoo.com>
2020-11-24 16:08:59 -0500

    Created spring boot project

---
+++ src/test/java/io/practise/jdbcapp/JdbcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.practise.jdbcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
