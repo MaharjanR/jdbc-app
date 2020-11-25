69227f368a8c6672b4b596958a19d7d974d657ea
Ritesh Maharjan <rite_maharjan@yahoo.com>
2020-11-25 09:06:06 -0500

    Added customer mapper and exceptions

---
+++ src/main/java/io/practise/jdbcapp/exceptions/UserNotFoundExceptions.java
@@ -0,0 +1,2 @@
+package io.practise.jdbcapp.exceptions;public class UserNotFoundExceptions {
+}
---
+++ src/main/java/io/practise/jdbcapp/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package io.practise.jdbcapp.mapper;
+
+import io.practise.jdbcapp.domain.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CustomMapper implements RowMapper<User> {
+
+
+    @Override
+    public User mapRow(ResultSet resultSet, int i) throws SQLException {
+        User user = new User();
+        user.setId(resultSet.getInt("user_id"));
+        user.setUserName(resultSet.getString("USERNAME"));
+        user.setUserPass(resultSet.getString("PASSWORD"));
+        user.setCreateDate(resultSet.getDate("CREATE_DATE"));
+        return user;
+    }
+}
